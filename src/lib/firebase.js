50c745714188d2689c51f928111cdbbc76d3ce78
Aryan Kaul <115831130+RajmaPlAyZ@users.noreply.github.com>
2024-09-30 13:51:05 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,13 +7,13 @@ import { getStorage } from 'firebase/storage';
 
 // Your Firebase configuration object
 const firebaseConfig = {
-    apiKey: "AIzaSyBxFlO4nMk4NXZg3oR7p4kNaQvLHIyPHO8",
-    authDomain: "secondrajma.firebaseapp.com",
-    projectId: "secondrajma",
-    storageBucket: "secondrajma.appspot.com",
-    messagingSenderId: "37960098241",
-    appId: "1:37960098241:web:e776265501ad4eacaacf40",
-    measurementId: "G-XGVMNBTJ8R"
+  apiKey: "AIzaSyBeTBUHOOXbwdpSeR-blvMY9r6mXX4zMQw",
+  authDomain: "thirdrajma.firebaseapp.com",
+  projectId: "thirdrajma",
+  storageBucket: "thirdrajma.appspot.com",
+  messagingSenderId: "5113873812",
+  appId: "1:5113873812:web:c3d98a1c8fe4b86459fb81",
+  measurementId: "G-1EGX8WC99Q"
   };
 
 // Initialize Firebase
